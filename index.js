741b1f63d2124858328c3d7351f8937002dbf564
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-08-17 20:08:57 +0700

    ok

--- index.js
+++ index.js
@@ -663,7 +663,7 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
     try {
-      await fastify.listen({ port: 3000, hostname: '0.0.0.0' })
+      await fastify.listen({ port: 3000, host: '0.0.0.0' })
     } catch (err) {
       fastify.log.error(err)
       process.exit(1)
