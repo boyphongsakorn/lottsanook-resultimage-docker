8242a8ce9c25cdb187f2b74173dbd8301e3e198f
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-11-16 17:21:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ fastify.get('/', async (request, reply) => {
             reply.type('image/jpeg');
             return buffer;
         }
-    } else if (!request.query.date) {
+    } else if (!request.query.date && Object.keys(request.query).length == 0) {
         const checkimage = await fetch('https://raw.githubusercontent.com/boyphongsakorn/testrepo/main/img_tmp/'+new Date().getDate()+''+(new Date().getMonth() + 1)+''+(new Date().getFullYear() + 543));
         const status = await checkimage.status;
         if (status == 200) {
