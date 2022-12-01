2432f35c58b9a01af49196f4cc41a4a54663c185
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-12-01 20:25:36 +0700

    ok

--- index.js
+++ index.js
@@ -542,14 +542,3 @@ const start = async () => {
 }
 
 start()
-
-const start = async () => {
-    try {
-      await fastify.listen({ port: 3000 })
-    } catch (err) {
-      fastify.log.error(err)
-      console.log(err)
-      process.exit(1)
-    }
-  }
-  start()
